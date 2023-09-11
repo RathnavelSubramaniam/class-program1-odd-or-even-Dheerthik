6a65732b82b21a07ce0a23347de0a92e3c336ea0
Dheerthik <142964411+Dheerthik@users.noreply.github.com>
2023-09-11 07:04:03 +0000

    oddoreven completed

---
+++ oddoreven.java
@@ -0,0 +1,11 @@
+class oddeven{
+    public static void main(String args[]){
+        int num=3;
+        if(num%2==0){
+            System.out.println("The given number is Odd");}
+            else{
+                System.out.println("The given number is Even");
+            }
+
+        }
+    }
